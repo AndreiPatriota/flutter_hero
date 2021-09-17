c6b0c2aa8b57ef924ddc2bd480176c40e3e48016
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2021-09-17 18:23:55 -0300

    Start populating graphics stack
    Just began to populate the stack of widgets that will hold the graphical objects that will be displayed on the main screen.
    
    Also needed to change the GameObject class to be a non-abstract class, so that it could be instantiated.

--- lib/GameCore.dart
+++ lib/GameCore.dart
@@ -1,1 +1,12 @@
-// TODO Implement this library.
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_hero/GameObject.dart';
+import 'package:flutter_hero/Player.dart';
+
+var gameLoopController;
+var screenWidth = 0.0;
+var screenHeight = 0.0;
+var score = 0;
+Player player;
+GameObject crystal;
+
+void firstInitialization(BuildContext context, dynamic widget){}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'InputController.dart' as InputController;
-import 'GameCore.dart';
+import 'GameCore.dart' as gcore;
 
 /*This is the game entry point*/
 void main() => runApp(FlutterHero());
 
-
 class FlutterHero extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     /*This is the build method*/
@@ -24,11 +22,59 @@ class FlutterHero extends StatelessWidget {
   }
 }
 
-class GameScreen extends StatefulWidget{
+class GameScreen extends StatefulWidget {
+  /*Initial screen*/
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  GameScreenState createState() => GameScreenState();
+}
+
+class GameScreenState extends State with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    /*This is the build method*/
+
+    //Initializes global parameters the first time it runs
+    if (gcore.gameLoopController == null)
+      gcore.firstInitialization(context, this);
+
+    //Assembles a list of Positioned widgets that will hold the graphics
+    //displayed on the main screen
+    List<Widget> stackChildren = [
+      Positioned(
+          //Background image
+          child: Container(
+        width: gcore.screenWidth,
+        height: gcore.screenHeight,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/background.png'), fit: BoxFit.cover)),
+      )),
+      Positioned(
+        //Score
+        child: Text(
+          'Score: ${gcore.score.toString().padLeft(4, '0')}',
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.w900),
+        ),
+        top: 2,
+        left: 4,
+      ),
+      Positioned(
+        //Linear progress bar
+        child: LinearProgressIndicator(
+          value: gcore.player.energy,
+          backgroundColor: Colors.white,
+          valueColor: AlwaysStoppedAnimation(Colors.red),
+        ),
+        top: 2,
+        left: 120,
+        width: gcore.screenWidth - 124,
+        height: 22,
+      ),
+      gcore.crystal.draw()
+    ];
+
+    return Container();
   }
 }
